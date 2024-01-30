5f3885b96cd25866eb2847bcd198b5e92e62b2aa
Amit Sahoo <amitsahoo1989@gmail.com>
2024-01-30 21:18:35 +0530

    Added the user input function

--- DAL/main.py
+++ DAL/main.py
@@ -3,46 +3,35 @@ import os
 import colorama
 
 from src.utilities import get_directory_path, query_yes_no
-from src.utilities import snowflake_connector as sc
-from src.utilities.exceptions import CustomException
 from src.cli import dynamic_sql_query, execute_query
 from src.cli import argument_parser
 
 colorama.init()
 
+
 def main():
-    relation_csv_dir_path = get_directory_path(path = str(os.path.dirname(__file__)),
-                                           levels=1,
-                                            directory_name = "tables_relationships.csv")
+    relation_csv_dir_path = get_directory_path(path=str(os.path.dirname(__file__)),
+                                               levels=1,
+                                               directory_name="tables_relationships.csv")
 
-    #args=argument_parser()
+    # args=argument_parser()
 
-    json_file = r"C:\Users\amit.sahoo\OneDrive - Argo Group\DAL\SOURCE_CODE\DAL\Data Samples\sample4.json"
+    json_file = r"C:\Users\Amit\PycharmProjects\pythonProject\sample\pythonProject\Prototype\DAL_POC\Data Samples\sample3.json"
 
-    sql_query = dynamic_sql_query(#args.json_file,
-                                  json_file,
-                                  relation_csv_dir_path)
-    
+    sql_query = dynamic_sql_query(  # args.json_file,
+        json_file,
+        relation_csv_dir_path)
+
     review_query = "\n\nGenerated SQL Query:\n\n\n" + "\33[33m" + sql_query + "\33[0m"
     sys.stdout.write(review_query)
 
-    sys.stdout.write(query_yes_no("\n\n Proceed to execute the query?"))
+    boolean_input = query_yes_no("\n\n Proceed to execute the query?")
 
-
-    
-    
-    # print(review_query)
-    # if review_query == 'y':
-    #         execute_query(sql_query)
-    # else:
-    #     sys.stdout.write("SQL query execution aborted.")
-
-
+    if boolean_input:
+        execute_query(sql_query)
+    else:
+        sys.stdout.write("SQL query execution aborted.")
 
 
 if __name__ == "__main__":
     main()
-    #print(sys.path)
-
-
-
