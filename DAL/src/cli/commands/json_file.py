027110071ac5afe7595eb4efcb697011f14b0068
amitsahoo11071989 <77171718+amitsahoo11071989@users.noreply.github.com>
2024-01-31 14:36:13 +0530

    Revert "for handling multiple joins"

--- DAL/src/cli/commands/json_file.py
+++ DAL/src/cli/commands/json_file.py
@@ -35,21 +35,11 @@ def generate_sql_query(json_data, relationships_df):
             levels=2,
             directory_name="templates"
         )
-        final_joining_conditions = []
-        for i in (0,len(json_tables)-2):
-            table1 = json_tables[i]
-            table2 = json_tables[i+1]
-            filtered_df = relevant_relationships_df[(relevant_relationships_df['full_name_table1'] == table1) & (relevant_relationships_df['full_name_table2'] == table2) | (relevant_relationships_df['full_name_table1'] == table2) & (relevant_relationships_df['full_name_table2'] == table1)]
-            #temp_df1 = relevant_relationships_df[(relevant_relationships_df['full_name_table1']==table1) & (relevant_relationships_df['full_name_table2']==table2)]
-            # temp_df2 = temp_df1[temp_df1['full_name_table2']==table2]
-            final_joining_conditions.append(' JOIN {} ON {}'.format(table2, filtered_df['join_condition'].values[0]))
-        print(final_joining_conditions)
+
         env = Environment(loader=FileSystemLoader(templates_directory_path))
         template = env.get_template('template_create_generator.jinja')
-        #print("\n",json_data,"\n",relevant_relationships_df)
+
         rendered_query = template.render(
-            final_joining_conditions = final_joining_conditions,
-            json_tables = json_tables,
             config=json_data,
             relevant_relationships_df=relevant_relationships_df
         )
