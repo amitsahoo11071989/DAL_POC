c7af976fa1ac8b1b9d78aefe73c5d689fa991400
amit.sahoo <amit.sahoo@argogroupus.com>
2024-01-31 15:48:04 +0530

    permutation is added

--- DAL/src/cli/commands/json_file.py
+++ DAL/src/cli/commands/json_file.py
@@ -5,7 +5,6 @@ from jinja2 import Environment, FileSystemLoader
 from src.utilities.utils import (
     get_directory_path,
     read_json,
-    get_full_relationship,
     read_csv,
     get_full_table_name)
 from src.utilities.exceptions import CustomException
@@ -30,14 +29,6 @@ def generate_sql_query(json_data, relationships_df):
         
         relevant_df = pd.merge(possible_combination_df, relationships_df, on=["full_name_table1", "full_name_table2"], how='inner')
         
-        # relevant_relationships_df = relationships_df[
-        #     (relationships_df['full_name_table1'].isin(json_tables)) &
-        #     (relationships_df['full_name_table2'].isin(json_tables))
-        #     ]
-
-        # if len(json_tables) == len(relevant_relationships_df):
-        #     relevant_relationships_df = relevant_relationships_df.drop(len(relevant_relationships_df) - 1, axis='index')
-
         templates_directory_path = get_directory_path(
             path=str(os.path.dirname(__file__)),
             levels=2,
@@ -49,7 +40,7 @@ def generate_sql_query(json_data, relationships_df):
 
         rendered_query = template.render(
             config=json_data,
-            relevant_relationships_df=relevant_relationships_df
+            relevant_df=relevant_df
         )
 
         return ' '.join(rendered_query.split('\n'))
