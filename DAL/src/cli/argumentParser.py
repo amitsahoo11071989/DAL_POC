47445b670d261df3366d64ad4c85b27bc4e90068
Pratik Sinha <pratiksinha@outlook.in>
2024-02-01 19:41:39 +0530

    Added the changes for cli arguments check.

--- DAL/src/cli/argumentParser.py
+++ DAL/src/cli/argumentParser.py
@@ -1,8 +1,12 @@
 import argparse
+import sys
 
 
 def argument_parser():
     parser = argparse.ArgumentParser(description='Database Query CLI')
-    parser.add_argument('-jf', '--json_file', type=str, help='Path to the JSON configuration file. Ex - /path/to/file.json')
+    parser.add_argument('-jf', '--json_file', type=str, help='Path to the JSON configuration file. Ex - /path/to/file.json. Example: python main.py -jf "<path of JSON configuration file>"')
+    if (len(sys.argv)<3) or (len(sys.argv)!=3) or (sys.argv[1] not in ['-jf', '--json_file']):
+        parser.print_help(sys.stderr)
+        sys.exit(0)
     args = parser.parse_args()
     return args
